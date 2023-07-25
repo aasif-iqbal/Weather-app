3fdfe53b01eb4b2fe0bb930da246507ce039d2b4
aasif-iqbal <aasif.iqbal3000@gmail.com>
2023-07-26 02:17:30 +0530

    testing api

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const request = require('request');
 const url = 'http://api.weatherstack.com/current?access_key=3f53595038b7f91a7234c7def7fcbeaf&query=37.8267,-122.4233&units=f';
 
 request({url:url, json:true}, function (error, response) {
-//    const data = JSON.parse(response.body);
+//    const dataObj = JSON.parse(response.body);
     // console.log(response.body.current);
     const current = response.body.current;
      
@@ -23,3 +23,11 @@ const showWeatherForcast = (current) => {
 // https://api.mapbox.com/geocoding/v5/mapbox.places/{search_text}.json
 
 // pk.eyJ1Ijoiam9obmRlbzg3ODkiLCJhIjoiY2xrNGk3eDdmMTFnMjNkbXBieGVsaWs5ZiJ9.fUo7HjRgsD5gyJgJDAJ99A
+
+/*
+As, This package has been deprecated 
+https://www.npmjs.com/package/request
+
+So, We can use postman-request
+https://www.npmjs.com/package/postman-request
+*/ 
