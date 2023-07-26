ebdf08679db32608d5f99e350e00d3fd1cc61fdd
aasif-iqbal <aasif.iqbal3000@gmail.com>
2023-07-27 03:08:31 +0530

    location added

--- app.js
+++ app.js
@@ -2,23 +2,7 @@ const request = require('request');
 
 // const url = 'http://api.weatherstack.com/current?access_key=22df95c17943bd664876c9c59c1620d1&query=37.8267,-122.4233';
 
-// convert temperature to units=f, temperature: Fahrenheit
-const url = 'http://api.weatherstack.com/current?access_key=3f53595038b7f91a7234c7def7fcbeaf&query=37.8267,-122.4233&units=f';
 
-request({url:url, json:true}, function (error, response) {
-//    const dataObj = JSON.parse(response.body);
-    // console.log(response.body.current);
-    const current = response.body.current;
-     
-    showWeatherForcast(current)    
-});
-
-const showWeatherForcast = (current) => {
-    
-    let weatherForcast = current.weather_descriptions[0]+': Current temperature is '+current.temperature+' degree and its feels like ' + current.feelslike;
-
-    console.log(weatherForcast);    //Sunny: Current temperature is 72 degree and its feels like 72
-}
 
 // mapBox api
 
@@ -43,6 +27,29 @@ const showWeatherForcast = (current) => {
     https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1Ijoiam9obmRlbzg3ODkiLCJhIjoiY2xrNGk3eDdmMTFnMjNkbXBieGVsaWs5ZiJ9.fUo7HjRgsD5gyJgJDAJ99A&limit=1;
 */
 
+// Weather-api
+
+// convert temperature to units=f, temperature: Fahrenheit
+const url = 'http://api.weatherstack.com/current?access_key=3f53595038b7f91a7234c7def7fcbeaf&query=37.8267,-122.4233&units=f';
+
+
+request({url:url, json:true}, function (error, response) {
+//    const dataObj = JSON.parse(response.body);
+    // console.log(response.body.current);
+    const current = response.body.current;
+     console.log(current);
+    showWeatherForcast(current)    
+});
+
+const showWeatherForcast = (current) => {
+    
+    let weatherForcast = current.weather_descriptions[0]+': Current temperature is '+current.temperature+' degree and its feels like ' + current.feelslike;
+
+    // console.log(weatherForcast);//Sunny: Current temperature is 72 degree and its feels like 72
+}
+
+
+
 /* Task: Print the lat/long for Los Angeles
 // 1. Fire off a new request to the URL explored in browser
 // 2. Have the request module parse it as JSON
@@ -50,7 +57,21 @@ const showWeatherForcast = (current) => {
 // 4. Test your work!
 */
 
-const geocoding = '';
+const geocoding = 'https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1Ijoiam9obmRlbzg3ODkiLCJhIjoiY2xrNGk3eDdmMTFnMjNkbXBieGVsaWs5ZiJ9.fUo7HjRgsD5gyJgJDAJ99A&limit=1';
+
+request({uri:geocoding, json:true}, function(error, response){
+    
+    if(error){
+        console.log(error);
+
+    } else{
+        // console.log('response',response.body);
+        const latitude = response.body.features[0].center[0];
+        const longitude = response.body.features[0].center[1];
+        // console.log(latitude, longitude);
+    }
+
+});
 
 
 /*
